6fac90123649d26fbaf51d59096ef1dd243611a4
mutugiii <mutugimutuma@gmail.com>
2022-02-14 15:46:16 +0300

    Wallet connect to dapp using thirdweb

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import { useEffect, useState, useMemo } from "react";
+
+import { useWeb3 } from "@3rdweb/hooks";
+
 const App = () => {
+  const { connectWallet, address, error, provider } = useWeb3();
+  console.log("Address: ", address);
+
+  // Wallet not connected
+  if(!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to TheDAO</h1>
+        <button onClick={() => connectWallet("injected")} className="btn-hero">
+          Connect your wallet
+        </button>
+      </div>
+    )
+  }
+
+  // Wallet connected
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>Wallet Connected, Welcome to My DAO 👀 </h1>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,10 +3,27 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App.jsx";
 
+import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
+
+// Supported chains - 4=rinkeby
+const supportedChainIds = [4];
+
+// Wallet to support - Metamask
+const connectors = {
+  injected: {},
+};
+
+
 // Render the App component to the DOM
+// Wrap app with 3rdweb provider
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThirdwebWeb3Provider
+      connectors={connectors}
+      supportedChainIds={supportedChainIds}
+    >
+      <App />
+    </ThirdwebWeb3Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
