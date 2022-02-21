5772108f11b71889fd402a434da4f8bec3efb653
mutugiii <mutugimutuma@gmail.com>
2022-02-21 17:30:06 +0300

    Initialize 3rdweb sdk

---
+++ scripts/1-initialize-sdk.js
@@ -0,0 +1,40 @@
+import { ThirdwebSDK } from "@3rdweb/sdk";
+import ethers from "ethers";
+
+// Handling env keys configuration
+import dotenv from "dotenv";
+dotenv.config();
+
+// Environmental keys error handling
+if(!process.env.PRIVATE_KEY && process.env.PRIVATE_KEY == "" ) {
+  console.log("🛑 Private key not found!");
+}
+
+if(!process.env.ALCHEMY_API_URL && process.env.ALCHEMY_API_URL == "") {
+  console.log("🛑 Alchemy API url not found!");
+}
+
+if(!process.env.WALLET_ADDRESS && process.env.WALLET_ADDRESS == "") {
+  console.log("🛑 Wallet Address not found!");
+}
+
+
+const sdk = new ThirdwebSDK(
+  new ethers.Wallet(
+    process.env.PRIVATE_KEY,
+    ethers.getDefaultProvider(process.env.ALCHEMY_API_URL),
+  ),
+);
+
+(async () => {
+  try {
+    const apps = await sdk.getApps();
+    console.log("Your app address is: ", apps[0].address);
+  } catch(err) {
+    console.error("Failed to get apps from the sdk", err);
+    process.exit(1);
+  }
+})()
+
+
+export default sdk;
