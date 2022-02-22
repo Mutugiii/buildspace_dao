f82fdf2476de0b2ff13ee9dbc1bcab5715f13b10
mutugiii <mutugimutuma@gmail.com>
2022-02-22 15:33:08 +0300

    Check if user has an NFT

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,48 @@
 import { useEffect, useState, useMemo } from "react";
 
 import { useWeb3 } from "@3rdweb/hooks";
+import { ThirdwebSDK } from "@3rdweb/sdk";
+
+// Instantiate sdk on rinkeby
+const sdk = new ThirdwebSDK("rinkeby");
+
+// Grab a ref to our erc-1115 contract.
+const bundleDropModule = sdk.getBundleDropModule(
+  "0x287f34F5c7084a14Ab96f16864D602459524b97b"
+);
 
 const App = () => {
   const { connectWallet, address, error, provider } = useWeb3();
   console.log("Address: ", address);
 
+  const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
+
+  useEffect(async () => {
+    // No wallet connected
+    if (!address) {
+      return;
+    }
+
+    // Check if user has NFT
+    const balance = await bundleDropModule.balanceOf(address, "0");
+
+    try {
+      // Check if bal is greater than 0
+      if(balance.gt(0)) {
+        setHasClaimedNFT(true);
+        console.log("🌟 this user has a membership NFT!");
+      } else {
+        setHasClaimedNFT(false);
+        console.log("😭 this user doesn't have a membership NFT.");
+      }
+    } catch (error) {
+      setHasClaimedNFT(false);
+      console.error("failed to nft balance", error);
+    }
+  }, [address]);
+
   // Wallet not connected
-  if(!address) {
+  if (!address) {
     return (
       <div className="landing">
         <h1>Welcome to TheDAO</h1>
@@ -15,7 +50,7 @@ const App = () => {
           Connect your wallet
         </button>
       </div>
-    )
+    );
   }
 
   // Wallet connected
